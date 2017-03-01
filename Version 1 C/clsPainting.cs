2cabb7377f81e9cbb7d2c016e793ca42eab2e701
Brandon Paul <yukimaster@outlook.com>
2017-03-01 17:00:34 +1300

    changed variable names in clsPainting

--- Version 1 C/clsPainting.cs
+++ Version 1 C/clsPainting.cs
@@ -6,9 +6,10 @@ namespace Version_1_C
     [Serializable()] 
     public class clsPainting : clsWork
     {
-        private float theWidth;
-        private float theHeight;
-        private string theType;
+        //changed these variable names
+        private float _Width;
+        private float _Height;
+        private string _Type;
 
         [NonSerialized()]
         private static frmPainting paintDialog;
@@ -19,10 +20,10 @@ namespace Version_1_C
             {
                 paintDialog = new frmPainting();
             }
-            paintDialog.SetDetails(_Name, theDate, theValue, theWidth, theHeight, theType);
+            paintDialog.SetDetails(_Name, theDate, theValue, _Width, _Height, _Type);
             if(paintDialog.ShowDialog() == DialogResult.OK)
             {
-               paintDialog.GetDetails(ref _Name, ref theDate, ref theValue, ref theWidth, ref theHeight, ref theType);
+               paintDialog.GetDetails(ref _Name, ref theDate, ref theValue, ref _Width, ref _Height, ref _Type);
             }
         }
     }
